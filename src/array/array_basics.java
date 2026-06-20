c92a03c88938d5d25273c1cc07303437bcde9341
udayan-kumar <udayanmannusingh01@gmail.com>
2026-06-20 13:44:46 +0530

    new question

---
+++ src/array/array_basics.java
@@ -0,0 +1,22 @@
+package src.array;
+
+public class array_basics {
+    public static void main(String[] args) {
+        // int[] x = {2,3,5,4,7,89,56};
+        // // indexing
+
+        // System.out.println(x[4]); // access
+
+        // // updating arrays and mutable property
+
+        // x[5] = 121;
+        // System.out.println(x[5]);
+        int [] arr = new int [4]; // 4  size ka array
+
+        arr[0] = 21;
+        arr[1] = 31;
+        arr[2] = 34;
+        arr[3] = 281;
+        
+    }
+}
