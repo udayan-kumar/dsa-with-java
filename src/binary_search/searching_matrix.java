cb4db75ed38b63586a6a2dc7cc6378c4a50c6638
udayan-kumar <udayanmannusingh01@gmail.com>
2026-07-04 17:20:01 +0530

    bineary searching of matrix

--- src/binary_search/searching_matrix.java
+++ src/binary_search/searching_matrix.java
@@ -5,23 +5,42 @@ public class searching_matrix {
         int[][] arr = {{1,6,12,27,32}, {38,41,47,49,55}, {67,68,75,81}, {84,89,91,92,98}};
         int n = arr.length;
         int m = arr[0].length;
-        int target = 555;
-        int row = 0;
-        int colm =  m-1;
+        int target = 41;
+        int row = arr.length;
+        int colm =  arr[0].length;
 
-        while(row<n && colm>=0){
-            if(arr[row][colm] > target){
-                colm--;
-            }
-            else 
-                if(arr[row][colm] < target){
-                    row++;
-                }
-                if(arr[row][colm] == target){
-                    System.out.println("true");
-                    break;
-                }
+        // while(row<n && colm>=0){
+        //     if(arr[row][colm] > target){
+        //         colm--;
+        //     }
+        //     else 
+        //         if(arr[row][colm] < target){
+        //             row++;
+        //         }
+        //         if(arr[row][colm] == target){
+        //             System.out.println("true");
+        //             break;
+        //         }
+        // }
+
+       int low = 0;
+       int high = row*colm-1;
+
+       while(low<=high){
+        int mid = (low + high)/2;
+        int rowmid = mid/colm, colmid = mid%colm;
+
+        if(arr[rowmid][colmid] == target){
+            System.out.println("true");
+            break;
         }
+        else 
+            if(arr[rowmid][colmid] > target){
+                high = mid - 1;
+            }
+            else
+                low = mid +1;
+       }
        
     }
 }
