50dae6c7bfff083f72a8b47cce287293ecb48393
udayan-kumar <udayanmannusingh01@gmail.com>
2026-07-10 08:36:15 +0530

    cyclic sorting by recursion

---
+++ src/cyclic_sort/sorting_array_recursion.java
@@ -0,0 +1,29 @@
+package src.cyclic_sort;
+
+import java.util.Arrays;
+
+public class sorting_array_recursion {
+    public static void main(String[] args) {
+         int[] arr = {3,5,9,2,1,4,8,10,6,7};
+         cyclic(arr);
+         System.out.println(Arrays.toString(arr));
+    }
+
+    public static void cyclic(int[] arr){
+        int i = 0;
+        while(i< arr.length){
+            int correctindex = arr[i] - 1;
+            if(arr[i] == arr[correctindex]) i++;
+            else {
+                swap(arr, i, correctindex);
+            }
+        }
+    }
+
+    public static void swap(int[] arr, int first, int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+
+    }
+}
