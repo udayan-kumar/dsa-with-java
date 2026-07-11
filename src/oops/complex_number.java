bf2028194887426433711190df4e0da3667481ca
udayan-kumar <udayanmannusingh01@gmail.com>
2026-07-11 13:01:09 +0530

    complex number

---
+++ src/oops/complex_number.java
@@ -0,0 +1,48 @@
+package src.oops;
+
+class complex_num{
+    int x;
+    int y;
+
+    complex_num(){
+
+    }
+
+    complex_num(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    void print(){
+        if(y>=0)System.out.println(x + " + i" + y);
+        else System.out.println(x +" -" +" i" +(-y));
+    }
+
+    void add(complex_num z){
+       x += z.x;
+       y += z.y;
+    }
+
+    void multiply(complex_num z){
+        x *= z.x;
+        y *= z.y;
+    }
+}
+public class complex_number {
+    public static void main(String[] args) {
+        complex_num z1 = new complex_num(2, 6);
+        z1.print();
+
+        complex_num z2 = new complex_num(3, -4);
+        z2.print();
+
+        // z1.add(z2);
+        // z1.print();
+        // z2.print();
+
+        z1.multiply(z2);
+         z1.print();
+        z2.print();
+    }
+}
+3:07
