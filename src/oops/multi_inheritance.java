a6fff6e0784ff78001f67ff64ed315db467a1870
udayan-kumar <udayanmannusingh01@gmail.com>
2026-07-12 08:26:20 +0530

    multi inheritance

---
+++ src/oops/multi_inheritance.java
@@ -0,0 +1,36 @@
+package src.oops;
+
+// class vehicle {
+//     int wheel;
+//     int speed;
+//     int seats;
+// }
+
+// class powervehicle extends vehicle {
+//     int engine;
+// }
+
+// class aircrafts extends powervehicle{
+//     int rotors;
+// }
+
+class animals{
+    int size;
+    boolean isvegitarian;
+
+}
+
+class birds extends animals{
+    int maximumaltitude;
+}
+
+class aquaticanimals extends animals{
+    int fins;
+}
+
+
+public class multi_inheritance {
+    public static void main(String[] args) {
+        
+    }
+}
