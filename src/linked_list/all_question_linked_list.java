9395936d87fb3412c11db2883a3e133c1f5a204f
udayan-kumar <udayanmannusingh01@gmail.com>
2026-07-19 10:32:36 +0530

    detect loop or not

--- src/linked_list/all_question_linked_list.java
+++ src/linked_list/all_question_linked_list.java
@@ -487,6 +487,20 @@ class linkedlist{  // user deffined data structure
         return arr.get(0);
     }
 
+    boolean detect_loop(Node head){
+        Node slow = head;
+        Node fast = head;
+
+        while(fast != null && fast.next!=null && fast.next.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(slow == fast) return true;
+           
+        }
+         return false;
+    }
+
 
      
 }
