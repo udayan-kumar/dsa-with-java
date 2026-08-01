3299ca117c3f6878fc37ff6fa7818d9953efc0e2
udayan-kumar <udayanmannusingh01@gmail.com>
2026-08-01 11:50:49 +0530

    stock span problem

---
+++ src/stacks/stock_span.java
@@ -0,0 +1,35 @@
+package src.stacks;
+
+import java.util.*;
+
+public class stock_span {
+    public static void main(String[] args) {
+        int[] arr = {100,80,90,100,60,75,85};
+        System.out.println(span(arr));
+    }
+
+    static ArrayList<Integer> span(int[] arr){
+        Stack <Integer> st = new Stack<>();
+        
+        int n = arr.length;
+        int[] s = new int[n];
+
+        s[0] = 1;
+        st.push(0);
+
+        for(int i = 1; i<n; i++){
+            while(st.size()>0 && arr[st.peek()]<=arr[i])st.pop();
+
+            if(st.size()==0) s[i] = i - (-1);
+            else{
+                s[i] = i - st.peek();
+            }
+            st.push(i);
+        }
+        ArrayList <Integer> ans = new ArrayList<>();
+        for(int i =0; i<n ; i++){
+            ans.add(s[i]);
+        }
+        return ans;
+    }
+}
