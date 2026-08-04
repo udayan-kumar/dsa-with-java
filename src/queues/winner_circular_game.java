7bfabc148a96aed0384f756fba9673d01ceb66e7
udayan-kumar <udayanmannusingh01@gmail.com>
2026-08-04 19:34:41 +0530

    winner of circular game

---
+++ src/queues/winner_circular_game.java
@@ -0,0 +1,33 @@
+package src.queues;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class winner_circular_game {
+    public static void main(String[] args) {
+        Queue <Integer> q = new LinkedList<>();
+
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+        q.add(6);
+        q.add(7);
+        q.add(8);
+        q.add(9);
+        q.add(10);
+        q.add(11);
+
+        int k = 4;
+
+        while(q.size()>1){
+            for(int i = 1; i<=k-1; i++){
+                q.add(q.remove());
+            }
+            q.remove();
+        }
+
+        System.out.println(q.peek());
+    }
+}
