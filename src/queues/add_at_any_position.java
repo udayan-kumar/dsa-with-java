47bf84152f7b496090f5053d3a91ca7e97f4aec3
udayan-kumar <udayanmannusingh01@gmail.com>
2026-08-04 18:14:36 +0530

    add at any position

---
+++ src/queues/add_at_any_position.java
@@ -0,0 +1,34 @@
+package src.queues;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class add_at_any_position {
+    public static void main(String[] args) {
+        Queue <Integer> q = new LinkedList<>();
+
+        q.add(10);
+        q.add(20);
+        q.add(30);
+        q.add(40);
+        q.add(50);
+        q.add(60);
+        q.add(70);
+
+        add(q, 3);
+        
+        System.out.println(q);
+    }
+
+    static void add(Queue <Integer> q, int index){
+        int n = q.size();
+        for(int i = 0; i<index; i++){
+            q.add(q.remove());
+
+        }
+        q.add(100);
+        for(int i = 0; i<n-index; i++){
+            q.add(q.remove());
+        }
+    }
+}
