1e287520019b0ff3589ffd6d8219bb8ecb7ab05f
udayan-kumar <udayanmannusingh01@gmail.com>
2026-08-08 19:30:47 +0530

    printing binary tree

---
+++ src/binary_tree/implementation.java
@@ -0,0 +1,39 @@
+package src.binary_tree;
+
+class Node{
+    int val;
+    Node left;
+    Node right;
+
+    Node(int val){
+        this.val = val;
+    }
+}
+public class implementation {
+    public static void main(String[] args) {
+       Node a = new Node(1);  // root
+       Node b = new Node(2);
+       Node c = new Node(3);
+       Node d = new Node(4);
+       Node e = new Node(5);
+       Node f = new Node(6);
+       Node g = new Node(7);
+       Node h = new Node(8);
+       Node i = new Node(9);
+
+       a.left = b; a.right = c;
+       b.left = d; b.right = e;
+       c.left = f; c.right = g;
+       d.left = h; d.right = i;
+
+       display(a);
+    }
+
+    private static void display(Node root){
+        if(root == null) return;
+
+        System.out.print(root.val + " ");
+        display(root.left);  // left ki sari value print kar dega
+        display(root.right);  // right ki sari value print kar dega
+    }
+}
