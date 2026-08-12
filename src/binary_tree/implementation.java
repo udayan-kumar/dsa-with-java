9b49d21a24074cde39deb33fead7d10915ec755c
udayan-kumar <udayanmannusingh01@gmail.com>
2026-08-12 22:12:19 +0530

    level order traversal

--- src/binary_tree/implementation.java
+++ src/binary_tree/implementation.java
@@ -1,5 +1,8 @@
 package src.binary_tree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 import src.oops.private_keyword;
 import src.two_D_array.sum;
 
@@ -29,144 +32,159 @@ public class implementation {
        c.left = f; c.right = g;
        d.left = h; d.right = i;
 
-    //    preorder_display(a);
-    //    System.out.println();
+       preorder_display(a);
+       System.out.println();
 
-    //    inorder_display(a);
-    //    System.out.println();
+       inorder_display(a);
+       System.out.println();
 
-    //    postorder_display(a);
-    //    System.out.println();
+       postorder_display(a);
+       System.out.println();
 
-    //    System.out.println(size(a));
+       System.out.println(size(a));
 
-    //    System.out.println(sum(a));
+       System.out.println(sum(a));
 
-    //    System.out.println(product(a));
+       System.out.println(product(a));
 
-    //    System.out.println(max(a));
+       System.out.println(max(a));
 
-    //    System.out.println(min(a));
+       System.out.println(min(a));
 
-    //    System.out.println(level(a));
+       System.out.println(level(a));
 
-    //    invert_binary_tree(a);
-    //    System.out.println();
+       invert_binary_tree(a);
+       System.out.println();
 
-    //    System.out.println(is_identical(a, a));
+       System.out.println(is_identical(a, a));
 
-    //    System.out.println(is_symmetric(a));
+       System.out.println(is_symmetric(a));
 
-    //    System.out.println(has_path_su(a, 20));
+       System.out.println(has_path_su(a, 20));
+
+       level_order_traversal(a);
     }
 
-    // private static void preorder_display(Node root){
-    //     if(root == null) return;
+    private static void preorder_display(Node root){
+        if(root == null) return;
 
-    //     System.out.print(root.val + " ");
-    //     preorder_display(root.left);  // left ki sari value print kar dega
-    //     preorder_display(root.right);  // right ki sari value print kar dega
-    // }
+        System.out.print(root.val + " ");
+        preorder_display(root.left);  // left ki sari value print kar dega
+        preorder_display(root.right);  // right ki sari value print kar dega
+    }
 
-    // private static void inorder_display(Node root){
-    //     if(root == null) return;
+    private static void inorder_display(Node root){
+        if(root == null) return;
 
-    //     inorder_display(root.left);
-    //     System.out.print(root.val + " ");
-    //     inorder_display(root.right);
-    // }
+        inorder_display(root.left);
+        System.out.print(root.val + " ");
+        inorder_display(root.right);
+    }
 
-    // private static void postorder_display(Node root){
-    //     if(root == null) return;
+    private static void postorder_display(Node root){
+        if(root == null) return;
 
-    //     postorder_display(root.left);
-    //     postorder_display(root.right);
-    //     System.out.print(root.val + " ");
-    // }
+        postorder_display(root.left);
+        postorder_display(root.right);
+        System.out.print(root.val + " ");
+    }
 
-    // private static int size(Node root){
-    //     if(root == null) return 0;
+    private static int size(Node root){
+        if(root == null) return 0;
 
-        //return 1 + size(root.left) + size(root.right);
+        return 1 + size(root.left) + size(root.right);
 
 
-    //     int leftsize = size(root.left);
-    //     int rightsize = size(root.right);
+        // int leftsize = size(root.left);
+        // int rightsize = size(root.right);
 
-    //     return 1 + leftsize + rightsize;
-    // }
+        // return 1 + leftsize + rightsize;
+    }
 
-    // private static int sum(Node root){
-    //     if(root ==  null) return 0;
+    private static int sum(Node root){
+        if(root ==  null) return 0;
 
-    //     return root.val + sum(root.left) + sum(root.right);
-    // }
+        return root.val + sum(root.left) + sum(root.right);
+    }
 
-    // private static int product(Node root){
-    //     if(root == null) return 1;
+    private static int product(Node root){
+        if(root == null) return 1;
 
-    //     return root.val * product(root.left) * product(root.right);
-    // }
+        return root.val * product(root.left) * product(root.right);
+    }
 
-    // private static int max(Node root){
-    //     if(root == null) return Integer.MIN_VALUE;
+    private static int max(Node root){
+        if(root == null) return Integer.MIN_VALUE;
 
-    //     return Math.max(root.val, Math.max(max(root.left), max(root.right)));
-    // }
+        return Math.max(root.val, Math.max(max(root.left), max(root.right)));
+    }
 
-    // private static int min(Node root){
-    //     if(root == null) return Integer.MAX_VALUE;
+    private static int min(Node root){
+        if(root == null) return Integer.MAX_VALUE;
 
-    //     return Math.min(root.val,Math.min(min(root.left), min(root.right)));
-    // }
+        return Math.min(root.val,Math.min(min(root.left), min(root.right)));
+    }
 
-    // private static int level(Node root){
-    //     if(root == null) return 0;
+    private static int level(Node root){
+        if(root == null) return 0;
 
-    //     return 1 + Math.max(level(root.left), level(root.right));
-    // }
+        return 1 + Math.max(level(root.left), level(root.right));
+    }
 
-    // private static void invert_binary_tree(Node root){
-    //     if(root == null) return;
+    private static void invert_binary_tree(Node root){
+        if(root == null) return;
 
-    //     invert_binary_tree(root.right);
-    //     invert_binary_tree(root.left);
-
-    //     System.out.print(root.val + " ");
-
-        // Node temp = root.left;
-        // root.left = root.right;
-        // root.right = temp;
-
-        // invert_binary_tree(root.left);
         // invert_binary_tree(root.right);
+        // invert_binary_tree(root.left);
+
         // System.out.print(root.val + " ");
 
-    // }
+        Node temp = root.left;
+        root.left = root.right;
+        root.right = temp;
 
-    // private static boolean is_identical(Node p, Node q){
-    //     if(p==null && q==null) return true;
-    //     if(p==null || q==null) return false;
-    //     if(p.val != q.val) return false;
+        invert_binary_tree(root.left);
+        invert_binary_tree(root.right);
+        System.out.print(root.val + " ");
 
-    //     return is_identical(p.left, q.left) && is_identical(p.right, q.right);
-    // }
+    }
 
-    // private static boolean is_symmetric(Node root){
-    //     if(root.left.val == root.right.val) return true;
-    //     if(root.left == null && root.right == null) return true;
-    //     if(root.left.val != root.right.val) return false;
+    private static boolean is_identical(Node p, Node q){
+        if(p==null && q==null) return true;
+        if(p==null || q==null) return false;
+        if(p.val != q.val) return false;
 
-    //     return is_symmetric(root.left) && is_symmetric(root.right);
-    // }
+        return is_identical(p.left, q.left) && is_identical(p.right, q.right);
+    }
 
-    // private static boolean has_path_su (Node root, int target_sum){
-    //     if(root==null) return false;
-    //     if(root.left == null && root.right == null){
-    //         if(target_sum == root.val) return true;
-    //         else return false;
-    //     }
+    private static boolean is_symmetric(Node root){
+        if(root.left.val == root.right.val) return true;
+        if(root.left == null && root.right == null) return true;
+        if(root.left.val != root.right.val) return false;
+
+        return is_symmetric(root.left) && is_symmetric(root.right);
+    }
+
+    private static boolean has_path_su (Node root, int target_sum){
+        if(root==null) return false;
+        if(root.left == null && root.right == null){
+            if(target_sum == root.val) return true;
+            else return false;
+        }
         
-    //     return has_path_su(root.left, target_sum-root.val) && has_path_su(root.right, target_sum-root.val);
-    // }
+        return has_path_su(root.left, target_sum-root.val) && has_path_su(root.right, target_sum-root.val);
+    }
+
+    private static void level_order_traversal(Node root){
+        Queue <Node> q = new LinkedList<>();
+        q.add(root);
+
+        while(q.size()>0){
+            Node front = q.remove();
+            System.out.print(front.val + " ");
+            if(front.left!=null)q.add(front.left);
+            if(front.right!=null)q.add(front.right);
+        }
+        System.out.println();
+    }
 }
